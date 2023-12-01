dcc3483368c4a6906b3167dbd79902255bdcb454
drewspitsin <spitsin92.92@gmail.com>
2023-12-02 02:48:15 +0300

    7th+

--- internal/service/auth/update.go
+++ internal/service/auth/update.go
@@ -3,13 +3,10 @@ package auth
 import (
 	"context"
 
-	"github.com/drewspitsin/auth/internal/logger"
 	"github.com/drewspitsin/auth/internal/model"
-	"go.uber.org/zap"
 )
 
 func (s *serv) Update(ctx context.Context, info *model.UserUpdate) error {
-	logger.Info("Creating...", zap.Int64("id", info.ID))
 	err := s.authRepository.Update(ctx, info)
 	if err != nil {
 		return err
