4cc58458f46b06e13150a52f513dda7902e35f2d
drewspitsin <spitsin92.92@gmail.com>
2023-12-02 02:45:10 +0300

    7th+

--- internal/service/auth/get.go
+++ internal/service/auth/get.go
@@ -3,13 +3,10 @@ package auth
 import (
 	"context"
 
-	"github.com/drewspitsin/auth/internal/logger"
 	"github.com/drewspitsin/auth/internal/model"
-	"go.uber.org/zap"
 )
 
 func (s *serv) Get(ctx context.Context, id int64) (*model.User, error) {
-	logger.Info("Creating...", zap.Int64("id", id))
 	user, err := s.authRepository.Get(ctx, id)
 	if err != nil {
 		return nil, err
